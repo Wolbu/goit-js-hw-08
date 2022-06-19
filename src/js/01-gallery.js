763e2e60c6016df500afd8e5c6e0fabbc4e3acc1
Wolbu <94459033+Wolbu@users.noreply.github.com>
2022-06-19 11:40:03 +0300

    exercise 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(element => {
+    return `<a class="gallery__item" href=${element.original}>
+  <img class="gallery__image" src=${element.preview} alt=${element.description} />
+</a>`;
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
