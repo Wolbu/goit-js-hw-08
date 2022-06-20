966ce91560c2d5a1172ee724cac92198cd27ee1a
Wolbu <94459033+Wolbu@users.noreply.github.com>
2022-06-20 20:37:57 +0300

    homework done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+SECONDS_KEY = 'videoplayer-current-time';
 
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+onPageLoading();
+
+player.on('timeupdate', throttle(rememberSecconds, 1000));
+
+function rememberSecconds(data) {
+  const time = data.seconds;
+  localStorage.setItem(SECONDS_KEY, data.seconds);
+}
+
+function onPageLoading() {
+  const savedTime = localStorage.getItem(SECONDS_KEY);
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const EMAIL_KEY = 'email';
+const MESSAGE_KEY = 'message';
+const emailInput = document.querySelector('input[ type="email"]');
+const messageInput = document.querySelector('textarea');
+const form = document.querySelector('.feedback-form');
+
+onPageLoading();
+
+emailInput.addEventListener('input', throttle(onEmailInput, 500));
+messageInput.addEventListener('input', throttle(onMessageInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onEmailInput(event) {
+  const userEmail = event.target.value;
+  localStorage.setItem(EMAIL_KEY, userEmail);
+}
+
+function onMessageInput(event) {
+  const userMessage = event.target.value;
+  localStorage.setItem(MESSAGE_KEY, userMessage);
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  form.reset();
+  localStorage.removeItem(EMAIL_KEY);
+  localStorage.removeItem(MESSAGE_KEY);
+}
+
+function onPageLoading() {
+  const savedEmail = localStorage.getItem(EMAIL_KEY);
+  if (savedEmail) {
+    emailInput.value = savedEmail;
+  }
+  const savedMessage = localStorage.getItem(MESSAGE_KEY);
+  if (savedMessage) {
+    messageInput.value = savedMessage;
+  }
+}
